e0cc11c715e3927750d49f0ae1977a90e4592094
Daniel <dte.biotech46@gmail.com>
2025-12-14 23:34:00 -0500

    set up temporary timer

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import time
 
 # Purpose: to run a stopwatch for the user's task
 # Parameters: task
 # Return: TBD
 def stopwatch():
-    pass
+    print("\nYour time starts now!") # message to the user
+
+    for seconds in range(1, 11): # temporary
+        time.sleep(1) # delay for 1 second acts as 1 second passing in the stopwatch
 
 # Purpose: call other functions, get task input
 # Parameters: none
@@ -11,4 +15,3 @@ def stopwatch():
 def main():
     task_name = input("What task do you want to do?: ") # task input
 
-    
