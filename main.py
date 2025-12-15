1349066ae8fa07bfe39a4c9bbcbec17ba96645e5
Daniel <dte.biotech46@gmail.com>
2025-12-15 00:39:55 -0500

    Moved the stop_status input into main.py

--- main.py
+++ main.py
@@ -1,35 +1,30 @@
 import time
 
-# Purpose: to read the stopwatch stop status from killswitch.yaml
-# Parameters: yaml_file
-# Return: stop_value
-def read_killswitch(yaml_file):
-    stop_status = yaml_file.read() # read the yaml file
-
-    status_list = stop_status.split(": ")
-
-    stop_value = status_list[1]
-
-    return stop_value
-
 # Purpose: to run a stopwatch for the user's task
 # Parameters: task
 # Return: seconds_used
 def stopwatch():
     print("\nYour time starts now!") # message to the user
 
-    file = open("killswitch.yaml", "r") # open the killswitch file
-
-    stop = read_killswitch(file) # call the reading function
+    stop_status = input("Do you want to stop the stopwatch? [yes/no]: ") # stop status prompt
 
     seconds_used = 0 # initialize  
 
-    while stop != "yes": # stopwatch continues until the user wants to stop
+    while stop_status != "yes": # stopwatch continues until the user wants to stop
         time.sleep(1) # delay for 1 second acts as 1 second passing in the stopwatch
 
         seconds_used += 1 # tracks how long it took for the user to do a task
+        
+        if seconds_used % 600 == 0: 
+            '''
+            to ensure the user does not have to constantly input 
+            while they're doing tasks and to ensure the user 
+            does not abuse the prompt to end early and procrastinate 
+            '''
+            
+            stop_status = input("Do you want to stop the stopwatch? [yes/no]: ") # stop status prompt
 
-        stop = read_killswitch(file) # update the stop value
+    print("Stopwatch has been stopped")
 
     return seconds_used
 
@@ -40,5 +35,7 @@ def main():
     task_name = input("What task do you want to do?: ") # task input
 
     seconds = stopwatch()
+
+    print(seconds)
 
 main()
