d4145dc8fa586132cafb2b37160db3292773d25f
Daniel <dte.biotech46@gmail.com>
2025-12-14 19:39:27 -0500

    Created main.ipy

--- stop.sh
+++
@@ -1,5 +0,0 @@
-read -s "Do you want to stop the timer? [y/n]: " stop_status
-
-if [["$stop_status" == "y"]]; then
-    pass
-fi
--- task.sh
+++
@@ -1,5 +0,0 @@
-read -p "What task do you want to do?: " task
-
-for minute in {1..5}; do
-    sleep 60
-done
